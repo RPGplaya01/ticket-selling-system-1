cc69b032932f600e3306bca160030911d1e69d0f
marcuswkl <56200199+marcuswkl@users.noreply.github.com>
2020-06-23 01:23:56 +0800

    Create constructor and getter methods
    Create Sales constructor and getter methods for the attributes

--- src/com/moelester/ticketsellingsystem/Sales.java
+++ src/com/moelester/ticketsellingsystem/Sales.java
@@ -8,4 +8,32 @@ public class Sales {
     private int[] ticketCount = {0, 0};
     private double[] ticketProfit = {0, 0};
 
+    public Sales() {}
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getTicketCount(String type) {
+        if (type.equals("Daily")) {
+            return ticketCount[0];
+        } else if (type.equals("Yearly")) {
+            return ticketCount[1];
+        } else {
+            System.out.println("Invalid Type");
+            return 0;
+        }
+    }
+
+    public double getTicketProfit(String type) {
+        if (type.equals("Daily")) {
+            return ticketProfit[0];
+        } else if (type.equals("Yearly")) {
+            return ticketProfit[1];
+        } else {
+            System.out.println("Invalid Type");
+            return 0;
+        }
+    }
+
 }
