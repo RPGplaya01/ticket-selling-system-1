222b213b96f18aea0f41b36d97884b612b389a98
marcuswkl <56200199+marcuswkl@users.noreply.github.com>
2020-06-23 01:22:17 +0800

    Create Sales class with attributes
    Create Sales.java with three attributes, import and use LocalDate package for first attribute, use arrays for second and third attributes

---
+++ src/com/moelester/ticketsellingsystem/Sales.java
@@ -0,0 +1,11 @@
+package com.moelester.ticketsellingsystem;
+
+import java.time.LocalDate;
+
+public class Sales {
+
+    private LocalDate date = LocalDate.now();
+    private int[] ticketCount = {0, 0};
+    private double[] ticketProfit = {0, 0};
+
+}
