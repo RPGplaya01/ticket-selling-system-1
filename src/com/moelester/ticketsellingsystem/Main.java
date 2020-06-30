b71bd1bf6f2fa65be71bb9a64638a060f99cafd0
marcuswkl <56200199+marcuswkl@users.noreply.github.com>
2020-06-30 12:09:42 +0800

    Implemented main method functionality (v1)
    Yearly ticket does not collect information yet

--- src/com/moelester/ticketsellingsystem/Main.java
+++ src/com/moelester/ticketsellingsystem/Main.java
@@ -1,20 +1,49 @@
 package com.moelester.ticketsellingsystem;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Ticket myTicket1 = new Yearly("Yearly","Senior");
-        Ticket myTicket2 = new Daily("Daily", "Kid/Student");
+        Sales sales = new Sales();
 
-        System.out.println("Instantiated ticket 1:");
-        System.out.println("Ticket type: " + myTicket1.getType());
-        System.out.println("Ticket category: " + myTicket1.getCategory());
-        System.out.println("Ticket price: " + myTicket1.getPrice());
+        Scanner keyboardInput = new Scanner(System.in);
 
-        System.out.println("\n\nInstantiated ticket 2:");
-        System.out.println("Ticket type: " + myTicket2.getType());
-        System.out.println("Ticket category: " + myTicket2.getCategory());
-        System.out.println("Ticket price: " + myTicket2.getPrice());
+        char repeat = 'N';
+
+        do {
+
+            System.out.println("Please select ticket type:");
+            String type = keyboardInput.nextLine();
+
+            System.out.println("Please select ticket category:");
+            String category = keyboardInput.nextLine();
+
+            System.out.println("Please input ticket count:");
+            int count = keyboardInput.nextInt();
+
+            if (type.equals("Daily")) {
+                Ticket ticket = new Daily(category);
+                double profit = ticket.getPrice() * count;
+                System.out.println("The total is RM" + profit);
+                sales.addTicketTransaction(type, count, profit);
+            }
+            else if (type.equals("Yearly")){
+                Ticket ticket = new Yearly(category);
+                double profit = ticket.getPrice() * count;
+                System.out.println("The total is RM" + profit);
+                sales.addTicketTransaction(type, count, profit);
+            }
+
+            System.out.println("Do you want to perform another transaction? (Y/N)");
+            repeat = keyboardInput.next().charAt(0);
+
+            // Consuming the leftover new line using the nextLine() method
+            keyboardInput.nextLine();
+
+        } while (repeat == 'Y');
+
+        sales.printReport();
 
     }
 }
