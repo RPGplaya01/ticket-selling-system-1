995ff281e715d3c4c54bd49645058c31f19dcce8
marcuswkl <56200199+marcuswkl@users.noreply.github.com>
2020-07-06 19:25:39 +0800

    Implemented CLI for Daily
    Added CLI for Daily ticket sales

--- src/com/moelester/ticketsellingsystem/Main.java
+++ src/com/moelester/ticketsellingsystem/Main.java
@@ -11,33 +11,107 @@ public class Main {
 
         char repeat = 'N';
 
+        System.out.println("                                     /$$$$$$$$                           /$$   /$$                                                                                                    \n" +
+                "                                    |_____ $$                           | $$$ | $$                                                                                                    \n" +
+                "                                         /$$/   /$$$$$$   /$$$$$$       | $$$$| $$  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$                                                    \n" +
+                "                                        /$$/   /$$__  $$ /$$__  $$      | $$ $$ $$ /$$__  $$ /$$__  $$ |____  $$ /$$__  $$|____  $$                                                   \n" +
+                "                                       /$$/   | $$  \\ $$| $$  \\ $$      | $$  $$$$| $$$$$$$$| $$  \\ $$  /$$$$$$$| $$  \\__/ /$$$$$$$                                                   \n" +
+                "                                      /$$/    | $$  | $$| $$  | $$      | $$\\  $$$| $$_____/| $$  | $$ /$$__  $$| $$      /$$__  $$                                                   \n" +
+                "                                     /$$$$$$$$|  $$$$$$/|  $$$$$$/      | $$ \\  $$|  $$$$$$$|  $$$$$$$|  $$$$$$$| $$     |  $$$$$$$                                                   \n" +
+                "                                    |________/ \\______/  \\______/       |__/  \\__/ \\_______/ \\____  $$ \\_______/|__/      \\_______/                                                   \n" +
+                "                                                                                             /$$  \\ $$                                                                                \n" +
+                "                                                                                            |  $$$$$$/                                                                                \n" +
+                "                                                                                             \\______/                                                                                 \n" +
+                " /$$$$$$$$ /$$           /$$                   /$$            /$$$$$$            /$$ /$$ /$$                            /$$$$$$                        /$$                            \n" +
+                "|__  $$__/|__/          | $$                  | $$           /$$__  $$          | $$| $$|__/                           /$$__  $$                      | $$                            \n" +
+                "   | $$    /$$  /$$$$$$$| $$   /$$  /$$$$$$  /$$$$$$        | $$  \\__/  /$$$$$$ | $$| $$ /$$ /$$$$$$$   /$$$$$$       | $$  \\__/ /$$   /$$  /$$$$$$$ /$$$$$$    /$$$$$$  /$$$$$$/$$$$ \n" +
+                "   | $$   | $$ /$$_____/| $$  /$$/ /$$__  $$|_  $$_/        |  $$$$$$  /$$__  $$| $$| $$| $$| $$__  $$ /$$__  $$      |  $$$$$$ | $$  | $$ /$$_____/|_  $$_/   /$$__  $$| $$_  $$_  $$\n" +
+                "   | $$   | $$| $$      | $$$$$$/ | $$$$$$$$  | $$           \\____  $$| $$$$$$$$| $$| $$| $$| $$  \\ $$| $$  \\ $$       \\____  $$| $$  | $$|  $$$$$$   | $$    | $$$$$$$$| $$ \\ $$ \\ $$\n" +
+                "   | $$   | $$| $$      | $$_  $$ | $$_____/  | $$ /$$       /$$  \\ $$| $$_____/| $$| $$| $$| $$  | $$| $$  | $$       /$$  \\ $$| $$  | $$ \\____  $$  | $$ /$$| $$_____/| $$ | $$ | $$\n" +
+                "   | $$   | $$|  $$$$$$$| $$ \\  $$|  $$$$$$$  |  $$$$/      |  $$$$$$/|  $$$$$$$| $$| $$| $$| $$  | $$|  $$$$$$$      |  $$$$$$/|  $$$$$$$ /$$$$$$$/  |  $$$$/|  $$$$$$$| $$ | $$ | $$\n" +
+                "   |__/   |__/ \\_______/|__/  \\__/ \\_______/   \\___/         \\______/  \\_______/|__/|__/|__/|__/  |__/ \\____  $$       \\______/  \\____  $$|_______/    \\___/   \\_______/|__/ |__/ |__/\n" +
+                "                                                                                                       /$$  \\ $$                 /$$  | $$                                            \n" +
+                "                                                                                                      |  $$$$$$/                |  $$$$$$/                                            \n" +
+                "                                                                                                       \\______/                  \\______/                                             ");
+
+        System.out.println("\nWelcome to Zoo Negara Ticket Selling System!");
+
+        System.out.println("     +--------------------+----------+-----------+\n" +
+                "     | Category           | Daily    | Yearly    |\n" +
+                "     +--------------------+----------+-----------+\n" +
+                "     | Senior             | 30       | 100       |\n" +
+                "     +--------------------+----------+-----------+\n" +
+                "     | Adult              | 50       | 120       |\n" +
+                "     +--------------------+----------+-----------+\n" +
+                "     | Kid/Student        | 20       | 80        |\n" +
+                "     +--------------------+----------+-----------+");
+
         do {
 
-            System.out.println("Please select ticket type:");
-            String type = keyboardInput.nextLine();
+        char typeChar;
+        String typeStr = null;
 
-            System.out.println("Please select ticket category:");
-            String category = keyboardInput.nextLine();
+        do {
 
-            System.out.println("Please input ticket count:");
-            int count = keyboardInput.nextInt();
+            System.out.println("\nPlease select the ticket type (1-2):");
+            System.out.println("1. Daily");
+            System.out.println("2. Yearly");
+            System.out.println("0. Exit");
 
-            if (type.equals("Daily")) {
-                Daily dailyTicket = new Daily(category);
-                double profit = dailyTicket.getPrice() * count;
-                System.out.println("The total is RM" + profit);
-                sales.addTicketTransaction(type, count, profit);
-            } else if (type.equals("Yearly")) {
-                Yearly yearlyTicket = new Yearly(category, "011111101111", "playa", "valorant", "m");
-                Yearly yearlyTicket2 = new Yearly(category, "011111102222", "playo", "csgo", "f");
-                yearlyTicket.printInfo();
-                yearlyTicket.writeInfo();
-                yearlyTicket2.printInfo();
-                yearlyTicket2.writeInfo();
-                double profit = yearlyTicket.getPrice() * count;
-                System.out.println("The total is RM" + profit);
-                sales.addTicketTransaction(type, count, profit);
+            typeChar = keyboardInput.next().charAt(0);
+
+            switch (typeChar) {
+                case '1' -> typeStr = "Daily";
+                case '2' -> typeStr = "Yearly";
+                case '0' -> System.exit(0);
+                default -> System.out.println("\nInvalid input! Please input only 1-2.");
             }
+
+        } while (typeChar != '1' && typeChar != '2');
+
+        System.out.println("\nYou have selected " + typeStr + " ticket type.");
+
+        int[] catCount = {0, 0, 0};
+
+        System.out.println("\nPlease enter the amount of tickets for each category:");
+        System.out.println("Senior: ");
+        catCount[0] = keyboardInput.nextInt();
+
+        System.out.println("Adult: ");
+        catCount[1] = keyboardInput.nextInt();
+
+        System.out.println("Kid/Student: ");
+        catCount[2] = keyboardInput.nextInt();
+
+        if (typeStr.equals("Daily")) {
+
+            double totalProfit = 0;
+            Daily seniorTicket = new Daily("Senior");
+            totalProfit += seniorTicket.getPrice() * catCount[0];
+            Daily adultTicket = new Daily("Adult");
+            totalProfit += adultTicket.getPrice() * catCount[1];
+            Daily kidTicket = new Daily("Kid/Student");
+            totalProfit += kidTicket.getPrice() * catCount[2];
+
+            int totalCount = catCount[0] + catCount[1] + catCount[2];
+            System.out.println("\nTicket Type: " + typeStr);
+            System.out.println("Ticket Quantity: " + totalCount);
+            System.out.println("Total Price: RM" + totalProfit);
+
+            sales.addTicketTransaction(typeStr, totalCount, totalProfit);
+
+        } else if (typeStr.equals("Yearly")) {
+//            Yearly yearlyTicket = new Yearly(category, "011111101111", "playa", "valorant", "m");
+//            Yearly yearlyTicket2 = new Yearly(category, "011111102222", "playo", "csgo", "f");
+//            yearlyTicket.printInfo();
+//            yearlyTicket.writeInfo();
+//            yearlyTicket2.printInfo();
+//            yearlyTicket2.writeInfo();
+//            double profit = yearlyTicket.getPrice() * count;
+//            System.out.println("The total is RM" + profit);
+//            sales.addTicketTransaction(type, count, profit);
+            System.out.println("Yearly Test");
+        }
 
             System.out.println("Do you want to perform another transaction? (Y/N)");
             repeat = keyboardInput.next().charAt(0);
