222c80bc00aee4ce65d429cb8d6a816f16e955d8
marcuswkl <56200199+marcuswkl@users.noreply.github.com>
2020-07-07 18:37:04 +0800

    Implemented new Sales methods
    Added obtainTransactionData() and writeTransaction()

--- src/com/moelester/ticketsellingsystem/Main.java
+++ src/com/moelester/ticketsellingsystem/Main.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Sales sales = new Sales();
-
         Scanner keyboardInput = new Scanner(System.in);
 
         char repeat;
@@ -36,6 +34,7 @@ public class Main {
 
         System.out.println("\nWelcome to Zoo Negara Ticket Selling System!");
 
+        System.out.println("Ticket Prices");
         System.out.println("     +--------------------+----------+-----------+\n" +
                 "     | Category           | Daily    | Yearly    |\n" +
                 "     +--------------------+----------+-----------+\n" +
@@ -48,120 +47,124 @@ public class Main {
 
         do {
 
-        char typeChar;
-        String typeStr = null;
-
-        do {
-
-            System.out.println("\nPlease select the ticket type (1-2):");
-            System.out.println("1. Daily");
-            System.out.println("2. Yearly");
-            System.out.println("0. Exit");
-
-            typeChar = keyboardInput.next().charAt(0);
-
-            switch (typeChar) {
-                case '1' -> typeStr = "Daily";
-                case '2' -> typeStr = "Yearly";
-                case '0' -> System.exit(0);
-                default -> System.out.println("\nInvalid input! Please input only 1-2.");
-            }
-
-        } while (typeChar != '1' && typeChar != '2');
-
-        System.out.println("\nYou have selected " + typeStr + " ticket type.");
-
-        if (typeStr.equals("Daily")) {
-
-            int[] catQuantity = {0, 0, 0};
-
-            System.out.println("\nPlease enter the amount of tickets for each category:");
-            System.out.println("Senior: ");
-            catQuantity[0] = keyboardInput.nextInt();
-
-            System.out.println("Adult: ");
-            catQuantity[1] = keyboardInput.nextInt();
-
-            System.out.println("Kid/Student: ");
-            catQuantity[2] = keyboardInput.nextInt();
-
-            double totalProfit = 0;
-            Daily seniorTicket = new Daily("Senior");
-            totalProfit += seniorTicket.getPrice() * catQuantity[0];
-            Daily adultTicket = new Daily("Adult");
-            totalProfit += adultTicket.getPrice() * catQuantity[1];
-            Daily kidTicket = new Daily("Kid/Student");
-            totalProfit += kidTicket.getPrice() * catQuantity[2];
-
-            int totalQuantity = catQuantity[0] + catQuantity[1] + catQuantity[2];
-
-        } else {
-
-            int quantity;
+            char typeChar;
+            String typeStr = null;
 
             do {
 
-                System.out.println("\nPlease enter the amount of tickets:");
-                quantity = keyboardInput.nextInt();
+                System.out.println("\nPlease select the ticket type (1-2):");
+                System.out.println("1. Daily");
+                System.out.println("2. Yearly");
+                System.out.println("0. Exit");
 
-                if (quantity > 3) {
-                    System.out.println("\nYou can only purchase 3 yearly tickets in one transaction.");
+                typeChar = keyboardInput.next().charAt(0);
+
+                switch (typeChar) {
+                    case '1' -> typeStr = "Daily";
+                    case '2' -> typeStr = "Yearly";
+                    case '0' -> System.exit(0);
+                    default -> System.out.println("\nInvalid input! Please input only 1-2.");
                 }
 
-            } while (quantity > 3);
+            } while (typeChar != '1' && typeChar != '2');
 
-            double totalProfit = 0;
+            System.out.println("\nYou have selected " + typeStr + " ticket type.");
 
-            for (int i = 1; i <= quantity; i++) {
+            if (typeStr.equals("Daily")) {
 
-                char catChar;
-                String catStr = null;
+                int[] catQuantity = {0, 0, 0};
 
-                System.out.println("\nPlease select the ticket category (1-3):");
-                System.out.println("1. Senior");
-                System.out.println("2. Adult");
-                System.out.println("3. Kid/Student");
+                System.out.println("\nPlease enter the amount of tickets for each category:");
+                System.out.println("Senior: ");
+                catQuantity[0] = keyboardInput.nextInt();
 
-                catChar = keyboardInput.next().charAt(0);
+                System.out.println("Adult: ");
+                catQuantity[1] = keyboardInput.nextInt();
 
-                switch (catChar) {
-                    case '1' -> catStr = "Senior";
-                    case '2' -> catStr = "Adult";
-                    case '3' -> catStr = "Kid/Student";
-                    default -> System.out.println("\nInvalid input! Please input only 1-3.");
+                System.out.println("Kid/Student: ");
+                catQuantity[2] = keyboardInput.nextInt();
+
+                double totalProfit = 0;
+                Daily seniorTicket = new Daily("Senior");
+                totalProfit += seniorTicket.getPrice() * catQuantity[0];
+                Daily adultTicket = new Daily("Adult");
+                totalProfit += adultTicket.getPrice() * catQuantity[1];
+                Daily kidTicket = new Daily("Kid/Student");
+                totalProfit += kidTicket.getPrice() * catQuantity[2];
+
+                int totalQuantity = catQuantity[0] + catQuantity[1] + catQuantity[2];
+
+                String transaction = Sales.obtainTransactionData(typeStr, totalQuantity, totalProfit);
+                System.out.println(transaction);
+                Sales.writeTransaction(transaction);
+
+            } else {
+
+                int quantity;
+
+                do {
+
+                    System.out.println("\nPlease enter the amount of tickets:");
+                    quantity = keyboardInput.nextInt();
+
+                    if (quantity > 3) {
+                        System.out.println("\nYou can only purchase 3 yearly tickets in one transaction.");
+                    }
+
+                } while (quantity > 3);
+
+                double totalProfit = 0;
+
+                for (int i = 1; i <= quantity; i++) {
+
+                    char catChar;
+                    String catStr = null;
+
+                    System.out.println("\nPlease select the ticket category (1-3):");
+                    System.out.println("1. Senior");
+                    System.out.println("2. Adult");
+                    System.out.println("3. Kid/Student");
+
+                    catChar = keyboardInput.next().charAt(0);
+
+                    switch (catChar) {
+                        case '1' -> catStr = "Senior";
+                        case '2' -> catStr = "Adult";
+                        case '3' -> catStr = "Kid/Student";
+                        default -> System.out.println("\nInvalid input! Please input only 1-3.");
+                    }
+
+                    // Consuming the leftover new line using the nextLine() method
+                    keyboardInput.nextLine();
+                    System.out.println("\nPlease enter the ID number e.g. 1234567890:");
+                    String idNum = keyboardInput.nextLine();
+
+                    System.out.println("\nPlease enter the name:");
+                    String name = keyboardInput.nextLine();
+
+                    System.out.println("\nPlease enter the address:");
+                    String address = keyboardInput.nextLine();
+
+                    System.out.println("\nPlease enter the gender:");
+                    String gender = keyboardInput.nextLine();
+
+                    Yearly yearlyTicket = new Yearly(catStr, idNum, name, address, gender);
+                    yearlyTicket.printInfo();
+                    yearlyTicket.writeInfo();
+
+                    totalProfit += yearlyTicket.getPrice();
+
                 }
 
-                keyboardInput.nextLine();
-                System.out.println("\nPlease enter the ID number e.g. 1234567890:");
-                String idNum = keyboardInput.nextLine();
-
-                System.out.println("\nPlease enter the name:");
-                String name = keyboardInput.nextLine();
-
-                System.out.println("\nPlease enter the address:");
-                String address = keyboardInput.nextLine();
-
-                System.out.println("\nPlease enter the gender:");
-                String gender = keyboardInput.nextLine();
-
-                Yearly yearlyTicket = new Yearly(catStr, idNum, name, address, gender);
-                yearlyTicket.printInfo();
-                yearlyTicket.writeInfo();
-
-                totalProfit += yearlyTicket.getPrice();
-
-                System.out.println("\nTicket Type: " + typeStr);
-                System.out.println("Ticket Quantity: " + i);
-                System.out.println("Total Price: RM" + totalProfit);
+                String transaction = Sales.obtainTransactionData(typeStr, quantity, totalProfit);
+                System.out.println(transaction);
+                Sales.writeTransaction(transaction);
 
             }
-
-        }
 
             System.out.println("Do you want to perform another transaction? (Y/N)");
             repeat = keyboardInput.next().charAt(0);
 
-            // Consuming the leftover new line using the nextLine() method
             keyboardInput.nextLine();
             repeat = Character.toUpperCase(repeat);
 
--- src/com/moelester/ticketsellingsystem/Sales.java
+++ src/com/moelester/ticketsellingsystem/Sales.java
@@ -7,6 +7,33 @@ import java.io.IOException;
 
 public class Sales {
 
-    
+    public static String obtainTransactionData(String type, int quantity, double profit) {
+        return "\nTransaction Date: " + LocalDate.now() +
+                "\nTicket Type: " + type +
+                "\nTicket Quantity: " + quantity +
+                "\nTotal Price: RM" + profit;
+    }
+
+    public static void writeTransaction(String transactionData) {
+        try {
+            File transaction = new File("transactions.txt");
+            if (transaction.createNewFile()) {
+                System.out.println("\ntransactions.txt is successfully created.");
+            }
+        } catch (IOException e) {
+            System.out.println("\nAn error has occurred when creating transactions.txt.");
+            e.printStackTrace();
+        }
+
+        try {
+            FileWriter transactionWriter = new FileWriter("transactions.txt", true);
+            transactionWriter.write(transactionData);
+            transactionWriter.close();
+            System.out.println("\nTransaction successfully written to transactions.txt.");
+        } catch (IOException e) {
+            System.out.println("\nAn error has occurred when writing transaction to transactions.txt.");
+            e.printStackTrace();
+        }
+    }
 
 }
